d42480c16a7f39b495479e5ba479cb2f339ab6a6
Darren Ngatimin <42824772+dngatimin@users.noreply.github.com>
2020-07-15 14:19:04 -0400

    Update Search & Sort.py

--- Search & Sort.py
+++ Search & Sort.py
@@ -1,5 +1,6 @@
 import random
-#search for val, if not then return None
+
+#different types of searches for val, if not then return None
 
 def linear_search(x,val):
     for i in range(len(x)):
@@ -20,26 +21,80 @@ def binary_search(x,val):
             return mid
     return None
 
-def insertion_sort():
+# different types of sort for list x
 
-def merge_sort(x, y):
-    i,j = 0
-    x[len(x)] = 10**10
-    y[len(y)] = 10**10
-    while i+j < len(x)+len(y):
-        if x[i] < y[j]:
-            z[i+j] = x[i]
-            i += 1
-        else:
-            z[i+j] = y[j]
-            j += 1
+def insertion_sort(x):
+    for i in range(1,len(x)):
+        j = i
+        temp = x[j]
+        while temp < x[j-1] and j > 0:
+            x[j] = x[j-1]
+            j -= 1
+        x[j] = temp
+    return x
+
+def merge_sort(x):
+    if len(x)>1:
+        l = x[:len(x)//2]
+        r = x[len(x)//2:]
+        i = j = k = 0
+
+        merge_sort(l)
+        merge_sort(r)
+
+        while i < len(l) and j < len(r):
+            if l[i] < r[j]:
+                x[k] = l[i]
+                i += 1
+            else:
+                x[k] = r[j]
+                j += 1
+            k+=1
+
+        while i < len(l): 
+            x[k] = l[i] 
+            i+= 1
+            k+= 1
+          
+        while j < len(r): 
+            x[k] = r[j] 
+            j+= 1
+            k+= 1
+    return x
             
-def bubble_sort():
-
-def heap_sort():
-
-def quick_sort(x):
-    pivot = random.randint(0,len(x)-1)
+def bubble_sort(x):
     for i in range(len(x)):
-        if
+        for j in range(0,len(x)-i-1):
+            if x[j] > x[j+1]:
+                x[j], x[j+1] = x[j+1],x[j]
+    return x
+
+def quick_sort(x, start, stop):
+    if(start < stop): 
+        pivot = pr(x, start, stop) 
+        quick_sort(x , start , pivot) 
+        quick_sort(x, pivot + 1, stop) 
+    return x
+
+def pr(x, start, stop): 
+    rpiv = random.randrange(start, stop) 
+    x[start], x[rpiv] = x[rpiv], x[start] 
+    return p(x, start, stop)
+
+def p(x,start,stop): 
+    temp = start
+    i = start - 1
+    j = stop + 1
+    while True: 
+        while True: 
+            i = i + 1
+            if x[i] >= x[temp]: 
+                break
+        while True: 
+            j = j - 1
+            if x[j] <= x[temp]: 
+                break
+        if i >= j: 
+            return j 
+        x[i] , x[j] = x[j] , x[i]
         
